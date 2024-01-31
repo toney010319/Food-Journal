94569185bf7f116b2ba62e2141d552bd3147d2db
Toney010319 <toney010319@gmail.com>
2024-02-01 03:22:52 +0800

    Creating recipe_controller for CRD and show

---
+++ backend/app/controllers/api/recipe_controller.rb
@@ -0,0 +1,37 @@
+module Api
+    class RecipeController < ApplicationController
+        def index
+            recipes = Recipe.all
+            render json: recipes
+        end
+
+    end
+
+    def create
+        recipe = Recipe.new(recipe_params)
+        if recipe.save
+            render json: recipe
+        else
+            render json: {error: 'Unable to create recipe.'}, status: 400
+        end
+    end
+
+    def destroy
+        recipe = Recipe.find(params[:id])
+        if recipe.destroy
+            render json: recipe
+        else
+            render json: {error: 'Unable to delete recipe.'}, status: 400
+        end
+    end
+
+    def show
+    recipe = Recipe.find(params[:id])
+    render json: recipe
+    end
+    
+    private
+    def recipe_params
+        params.require(:recipe).permit(:name, :ingredients)
+    end
+end
