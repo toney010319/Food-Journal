7dd6cc5a42c0f1b2a474ee66de4a7728ef585866
Gerome <gerome.pm@gmail.com>
2024-02-03 09:26:59 +0800

    Add flowbite plugin

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/lib/esm/**/*.js",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        text: "#0d0c02",
+        background: "#fdfded",
+        primary: "#ffee00",
+        secondary: "#6fc349",
+        accent: "#744c07",
+      },
+      fontSize: {
+        sm: "0.750rem",
+        base: "1rem",
+        xl: "1.333rem",
+        "2xl": "1.777rem",
+        "3xl": "2.369rem",
+        "4xl": "3.158rem",
+        "5xl": "4.210rem",
+      },
+      fontFamily: {
+        heading: "Poppins",
+        body: "Poppins",
+      },
+      fontWeight: {
+        normal: "400",
+        bold: "700",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
