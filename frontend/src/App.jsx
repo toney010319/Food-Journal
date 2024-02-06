dc1ba97c80d74aab11c234c031a81883518f8b99
Gerome <gerome.pm@gmail.com>
2024-02-07 04:23:19 +0800

    Fix app height.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -52,7 +52,7 @@ function AppContent() {
           <span className="text-xl font-body">{notice}</span>
         </Alert>
       )}
-      <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div className="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="w-full space-y-8">
           <Routes>
             <Route path="/login" element={<Login />} />
