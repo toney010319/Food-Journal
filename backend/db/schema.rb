5e54f300b53f64f865801216e7a5a6cc8625b723
Toney010319 <toney010319@gmail.com>
2024-02-12 20:45:43 +0800

    Creating Uri in recipes table

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -38,11 +38,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_08_181645) do
   end
 
   create_table "recipes", force: :cascade do |t|
-    t.string "label"
-    t.text "ingredientsLines", default: [], array: true
-    t.text "images", default: [], array: true
+    t.string "uri"
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -56,4 +56,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_08_181645) do
   end
 
   add_foreign_key "journals", "users"
+  add_foreign_key "recipes", "users"
 end
