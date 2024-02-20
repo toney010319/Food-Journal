976231d83a343e6dda40d957d945ea22e39034cd
Toney010319 <toney010319@gmail.com>
2024-02-21 00:38:05 +0800

    Troubleshoot Cors error

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "food-journal-eta.vercel.app"
+    origins 'food-journal-eta.vercel.app', 'localhost:3001', '127.0.0.1:3001', 'localhost:3000'
 
     resource "*",
       headers: :any,
